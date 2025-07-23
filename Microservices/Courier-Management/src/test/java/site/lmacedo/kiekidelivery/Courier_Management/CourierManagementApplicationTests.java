0ad1c937bb896e717ee05c42cfaf4eed29dc5ee7
lucaserm <lucasedumacedo@gmail.com>
2025-07-23 19:29:10 -0400

    feat: initial commit

---
+++ Microservices/Courier-Management/src/test/java/site/lmacedo/kiekidelivery/Courier_Management/CourierManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package site.lmacedo.kiekidelivery.Courier_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Microservices/Delivery-Tracking/src/test/java/site/lmacedo/kiekidelivery/delivery/tracking/DeliveryTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package site.lmacedo.kiekidelivery.delivery.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
