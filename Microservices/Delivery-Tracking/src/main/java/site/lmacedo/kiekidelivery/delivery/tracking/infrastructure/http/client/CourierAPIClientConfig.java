cc4c9fad8b64afb62fcc3ed613cb045bce6f6692
lucaserm <lucasedumacedo@gmail.com>
2025-07-24 09:31:48 -0400

    feat: implementing service discovery (eureka) with a gateway service

--- Microservices/Delivery-Tracking/src/main/java/site/lmacedo/kiekidelivery/delivery/tracking/infrastructure/http/client/CourierAPIClientConfig.java
+++ Microservices/Delivery-Tracking/src/main/java/site/lmacedo/kiekidelivery/delivery/tracking/infrastructure/http/client/CourierAPIClientConfig.java
@@ -1,6 +1,7 @@
 package site.lmacedo.kiekidelivery.delivery.tracking.infrastructure.http.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -9,12 +10,15 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 public class CourierAPIClientConfig {
-//    @Value("{api.courier.baseUrl}")
-    private String courierAPIBaseUrl = "localhost:3031";
+    @Bean
+    @LoadBalanced
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
 
     @Bean
     public CourierAPIClient courierAPIClient(RestClient.Builder builder) {
-        RestClient restClient = builder.baseUrl("http://" + courierAPIBaseUrl).build();
+        RestClient restClient = builder.baseUrl("http://courier-management").build();
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builderFor(adapter).build();
         return proxyFactory.createClient(CourierAPIClient.class);
---
+++ Microservices/Gateway/src/test/java/site/lmacedo/kiekidelivery/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package site.lmacedo.kiekidelivery.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
